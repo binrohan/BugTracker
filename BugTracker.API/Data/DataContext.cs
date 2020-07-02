a3afc6e470d843628b8ab509b472f45df5a10f46
Rohan-Bin-Khokon <binrohan97@gmail.com>
2020-07-02 14:42:55 +0600

    Identity Works

---
+++ BugTracker.API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.API.Data
+{
+    public class DataContext : DbContext
+    {
+        
+    }
+}
---
+++ BugTracker.API/Models/Value.cs
@@ -0,0 +1,9 @@
+namespace BugTracker.API.Models
+{
+    public class Value
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+        
+    }
+}
